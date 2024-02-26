f64ad1a994841378026742e5f779c92895f5cc7a
Prathyusha Gottamukkula <122482458+Prathu96@users.noreply.github.com>
2024-02-26 18:07:18 +0530

    Add files via upload

---
+++ to-do-App/to-do-list-redux/src/actions.js
@@ -0,0 +1,16 @@
+export function addTodo(todo) {
+    return { 
+        type: "ADD", 
+        payload: todo 
+    };
+  }
+  export function editTodo(todo) {
+    return { 
+        type: "EDIT", 
+        payload: todo };
+  }
+  export function deleteTodo(id) {
+    return { 
+        type: "DELETE", 
+        payload: id };
+  }
---
+++ to-do-App/to-do-list-redux/src/todos/TodoList.js
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+
+import '../index.css';
+import Todo from "../todos/Todo";
+
+
+function TodoList() {
+  const todos = useSelector((state) => state.todos);
+
+  console.log(todos);
+  return (
+    <ul className="list-group mt-4">
+      {todos.map((todo) => (
+        <Todo key={todo.id} id={todo.id} title={todo.title} />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
